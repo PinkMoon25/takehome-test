017ed60b77fd0ee7e89f6682e2fa62d90f1cfd4b
Tushar <singhtushar348@gmail.com>
2022-09-17 20:08:28 +0100

    Implement shuffle divs functionality

--- frontend/js/sort-and-shuffle.js
+++ frontend/js/sort-and-shuffle.js
@@ -1,19 +1,35 @@
 const sortBtn = document.getElementById('sort');
+const shuffleBtn = document.getElementById('shuffle');
 const container = document.querySelector('.div-container');
 const divs = document.querySelectorAll('.divs');
-const divarr = Array.from(divs);
+const divArr = Array.from(divs);
 
 function sortDiv(){
-    divarr.sort((a,b)=>{
+    divArr.sort((a,b)=>{
         if(a.textContent > b.textContent) return 1;
         if(a.textContent < b.textContent) return -1;
         if(a.textContent = b.textContent) return 0;
     });
 
-    divarr.forEach(div=>{
+    divArr.forEach(div=>{
         container.appendChild(div);
     })
 };
 
 sortBtn.addEventListener('click', sortDiv);
 
+function shuffleDiv(array){
+    for(let i=0; i<array.length; i++){
+        let j = Math.floor(Math.random()*array.length);
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    array.forEach(item=>{
+        container.appendChild(item);
+    })
+};
+
+shuffleBtn.addEventListener('click', ()=>{
+    shuffleDiv(divArr);
+});
